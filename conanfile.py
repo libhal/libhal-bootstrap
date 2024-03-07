61fe21086f4b10d8cd5f9fac022ef60469b50766
Khalil Estell <kammce0@gmail.com>
2024-03-07 07:00:31 -0800

    :bug: Make libhal-util header's transitive

--- conanfile.py
+++ conanfile.py
@@ -72,7 +72,7 @@ class demo:
 
 def add_library_requirements(conan_file: ConanFile):
     conan_file.requires("libhal/[^3.0.0]", transitive_headers=True)
-    conan_file.requires("libhal-util/[^4.0.0]")
+    conan_file.requires("libhal-util/[^4.0.0]", transitive_headers=True)
 
 
 class library:
@@ -170,5 +170,5 @@ class library_test_package:
 
 class libhal_bootstrap(ConanFile):
     name = "libhal-bootstrap"
-    version = "0.0.2"
+    version = "0.0.3"
     package_type = "python-require"
